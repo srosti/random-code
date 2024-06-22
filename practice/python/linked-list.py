4efcd029502533dd91ab6aecfc1e9578b8e62fbc
Shawn Rosti <shawn.rosti@gmail.com>
2024-06-22 08:33:47 -0700

    simple linked list
    TODO: add a remove and sort

---
+++ practice/python/linked-list.py
@@ -0,0 +1,28 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+        print(f"{current}")
+
+list = LinkedList()
+list.append(1)
+list.append(2)
+list.append(3)
+list.append(4)
