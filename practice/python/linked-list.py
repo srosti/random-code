9466f29921b3b4caf172ef832bb0d3cdab848919
Shawn Rosti <shawn.rosti@gmail.com>
2024-06-23 07:46:40 -0700

    added simple remove

--- practice/python/linked-list.py
+++ practice/python/linked-list.py
@@ -21,8 +21,40 @@ class LinkedList:
         current.next = new_node
         print(f"{current}")
 
+    def remove(self, data):
+
+        # The list is empty
+        if self.head is None:
+            return
+
+        # data matches the curent node, set the current node to then next
+        # which removes the reference. Python's garbage collector will reclaim memory
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        current = self.head
+        previous = None
+        while current:
+            if current.data == data:
+                break
+            previous = current
+            current = current.next
+
+        # The node wasn't found, do nothing
+        if current is None:
+            return
+
+        previous.next = current.next
+
+
+
+
+
 list = LinkedList()
 list.append(1)
 list.append(2)
 list.append(3)
 list.append(4)
+list.append(5)
+list.remove(2)
