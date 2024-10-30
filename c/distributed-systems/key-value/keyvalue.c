f73f0efe7474d5674ef0f1d0c3b2c9948b2b36ce
Shawn Rosti <shawn.rosti@gmail.com>
2024-10-29 19:14:36 -0700

    Add delete() and get()

--- c/distributed-systems/key-value/keyvalue.c
+++ c/distributed-systems/key-value/keyvalue.c
@@ -23,6 +23,17 @@ KeyValueStore *create_key_value_store(int capacity) {
 
 }
 
+char *get(KeyValueStore *store, const char *key) {
+    // Find the entry
+    for (int i=0; i < store->size; i++) {
+        if (strcmp(store->entries[i].key, key) == 0) {
+            return store->entries[i].value;
+        }
+    }
+    return NULL;
+
+}
+
 void put(KeyValueStore *store, const char *key, const char *value) {
 
     // Find existing entry if it exists
@@ -47,12 +58,33 @@ void put(KeyValueStore *store, const char *key, const char *value) {
 
 }
 
+void delete(KeyValueStore *store, const char *key) {
+
+    //find the entry to delete
+    for(int i=0; i < store->size; i++) {
+        if ( strcmp(store->entries[i].key, key)) {
+            free(store->entries[i].key);
+            free(store->entries[i].value);
+            // fill in the gap
+            for(int j=i; j < store->size-1; j++) {
+                store->entries[j] = store->entries[j+1];
+            }
+            store->size--;
+        }
+    }
+
+}
+
 int main() {
     KeyValueStore *keystore;
 
     keystore = create_key_value_store(10);
     put(keystore,"foo", "bar");
+    printf("foo=%s\n", get(keystore,"foo"));
     put(keystore,"jazz", "hands");
+    printf("jazz=%s\n", get(keystore,"jazz"));
     put(keystore,"foo", "baz");
+    delete(keystore,"baz");
+    printf("foo=%s\n", get(keystore,"foo"));
     return 0;
 }
